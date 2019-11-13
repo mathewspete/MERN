b816cd6af13fa03e93574cc2c9f4cbc2d4831a9f
mathews225 <petemathews14@gmail.com>
2019-11-13 09:30:03 -0500

    User input check

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Welcom to the Contact Keeper API.' }));
 
+// Define Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', require('./routes/users'));
@@ -10,4 +19,4 @@ app.use('/api/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
